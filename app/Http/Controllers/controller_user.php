db02df10bde73a4a331ec7ab761745de460523dd
Raynor111 <163610659+Raynor111@users.noreply.github.com>
2024-04-01 08:36:29 +0700

    a

---
+++ app/Http/Controllers/controller_user.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controller_user extends Controller
+{
+    public function bio_user()
+    {
+        return view('user/biodata_user');
+    }
+    public function halaman_login_user()
+    {
+        return view('user/halaman_login_user');
+    }
+    public function
+
+}
